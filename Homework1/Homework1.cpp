de5d8dc648fe34f4cc2dee0eb43a49742ddd4d20
Kirill <Kalininsky14@mail.ru>
2021-09-09 17:03:32 +0300

    Добавьте файлы проекта.

---
+++ Homework1/Homework1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    //task 1
+short int a{2};
+int b = 123123;
+long long c = 123123123;
+char d1 = 'k';
+char d2 = 'n';
+bool flag = true;
+flag = false;
+
+//task 2
+
+   enum Game { X, O, Empty, };
+
+//task 3
+
+    Game Field_arr[9] = { Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty };
+
+
+return 0;
+
+}
+
+
+
